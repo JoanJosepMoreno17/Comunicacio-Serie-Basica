6c7edfa913ccf8efa35cbc9132f7e5c6175e8af7
JoanJosepMoreno17 <joan.moreno.29@lacetania.cat>
2018-03-12 12:42:31 +0100

    activitats proposades

---
+++ activitat 4/missatge_caixer/missatge_caixer.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.println("  ");
+Serial.println("Escull el numero de l'operacio que vols realitzar?");
+Serial.println(" 1. Comprovar numero de tarjeta de credit");
+Serial.println(" 2. Comprovar numero de compte bancari");
+Serial.println(" 3. Buscar un digit perdut de tarjeta de credit");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
